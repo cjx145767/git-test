250d36513abcccb08c26299f8f2a269306ce6d2a
cjx <1457679876@qq.com>
2021-10-31 12:27:18 +0800

    hot fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello git3");
-        System.out.println("hello master");
+        System.out.println("hello maser");
     }
 }
